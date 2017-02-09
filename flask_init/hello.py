a1a20f421f78352210d273b01c9f6c1a4392d67d
bobbanovski <robert.coleman1@uqconnect.edu.au>
2017-02-09 14:31:11 +0000

    templates, conditional rendering

--- flask_init/hello.py
+++ flask_init/hello.py
@@ -1,12 +1,16 @@
 import os
+#import pdb; pdb.set_trace() #Use this to stop at a line then progress step by step.
+# Press n to advance, c to continue
+# print(i) to see value
 
-from flask import Flask
+from flask import Flask, request, render_template
 #Define instance of flask class
 app = Flask(__name__) #Ensures unique application name
 
 @app.route('/')
 def indexPage():
     return 'Welcome to CloudFlask'
+    #return url_for('usersPage', username='Robert') #need to import url_for
     
 @app.route('/users/<username>')
 def usersPage(username):
@@ -17,15 +21,16 @@ def usersPage(username):
 def posts(post_id):
     return 'Post with id: %d' % post_id
 
-
-@app.route('/hello') #return function helloWorld to person who accesses /
-def helloWorld():
-    i = 3
-    #import pdb; pdb.set_trace() #Use this to stop at a line then progress step by step.
-    # Press n to advance, c to continue
-    # print(i) to see value
-    i+=1
-    return 'Good Morning!' + " You've visited " + str(i) + ' times'
+@app.route('/hello')
+@app.route('/hello/<name>') #return function helloWorld to person who accesses /
+def hello(name=None): # None = not required
+    return render_template('hello.html', name_t = name)
+    
+@app.route('/login', methods=['GET', 'POST']) #Form submitted with GET method
+def login():
+    if request.method == 'POST':
+        return 'username entered was: %s' % request.values["username"] # %d already assigned to int
+    return '<form method="post" action="/login"><input type="text" name="username" /><br/><button type=submit>Submit</button></form>'
     
 #called from terminal, command line
 if __name__ == '__main__': 
