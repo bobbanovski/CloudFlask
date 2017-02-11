1cd11953e4c8a02ffccdc3ede432101827450e5f
bobbanovski <robert.coleman1@uqconnect.edu.au>
2017-02-11 05:33:30 +0000

    cookieAuth

--- flask_init/hello.py
+++ flask_init/hello.py
@@ -3,15 +3,10 @@ import os
 # Press n to advance, c to continue
 # print(i) to see value
 
-from flask import Flask, request, render_template, url_for, redirect, flash
+from flask import Flask, request, render_template, url_for, redirect, flash, make_response
 #Define instance of flask class
 app = Flask(__name__) #Ensures unique application name
 
-@app.route('/')
-def indexPage():
-    return 'Welcome to CloudFlask'
-    #return url_for('usersPage', username='Robert') #need to import url_for
-    
 @app.route('/users/<username>')
 def usersPage(username):
     visits = 3
@@ -32,7 +27,9 @@ def login():
     if request.method == 'POST':
         if (valid_login(request.form['username'], request.form['password'])): # %d already assigned to int
             flash("successfully logged in") #Stores on the session cookie, resets after retrieval
-            return redirect(url_for('welcome', username=request.form.get('username')))
+            response = make_response(redirect(url_for('welcome')))
+            response.set_cookie('username', request.form.get('username'))
+            return response
         else:
             error = 'incorrect username or password'
     return render_template('login.html', error=error)
@@ -43,9 +40,20 @@ def valid_login(username, password):
     else:
         return False
         
-@app.route('/welcome/<username>')
-def welcome(username):
-    return render_template('welcome.html', username=username)
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('login')))
+    response.set_cookie('username', '', expires=0) #Set the cookie's lifetime to 0
+    return response
+        
+@app.route('/')
+#def welcome(username):
+def welcome():
+    username = request.cookies.get('username')
+    if username:
+        return render_template('welcome.html', username=username)
+    else:
+        return redirect(url_for('login'))
     
 #called from terminal, command line
 if __name__ == '__main__': 
