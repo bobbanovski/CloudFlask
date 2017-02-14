5404f7ec354143145b11654486961eb10cde2726
bobbanovski <robert.coleman1@uqconnect.edu.au>
2017-02-14 03:19:22 +0000

    implemented authorRegisterForm w validation

---
+++ flask_init/author/form.py
@@ -0,0 +1,18 @@
+from flask_wtf import Form
+from wtforms import validators, StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+
+class RegisterForm(Form): #creates fields from 
+    fullname = StringField("Full Name", [validators.required()])
+    email = EmailField("Email", [validators.required()])
+    username = StringField("User Name", [
+        validators.required(),
+        validators.length(min=4,max=25)
+        ])
+    password = PasswordField("New Password", [
+        validators.required(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+        validators.length(min=4, max=80)
+        ])
+    confirm = PasswordField("Confirm Password")
+    
--- flask_init/author/views.py
+++ flask_init/author/views.py
@@ -1,5 +1,18 @@
 from flask_init import app
+from flask import render_template, redirect, url_for
+from author.form import RegisterForm
 
 @app.route('/login')
 def login():
     return 'hello author'
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return(url_for('registersuccess'))
+    return render_template('author/register.html', form=form)
+    
+@app.route('/registersuccess')
+def registersuccess():
+    return "Author registered successfully"
