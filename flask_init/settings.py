2eba9b88720678cbb652a30d7dd23fd02f3065d0
bobbanovski <robert.coleman1@uqconnect.edu.au>
2017-02-14 01:32:29 +0000

    added author class

--- flask_init/settings.py
+++ flask_init/settings.py
@@ -9,3 +9,4 @@ DB_NAME = 'cloudflaskdb'
 DB_HOST = os.getenv('IP', '0,0,0,0')
 DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URI #cannot change sqlalchemy variable name
+SQLALCHEMY_TRACK_MODIFICATIONS = True
