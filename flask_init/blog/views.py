00385d580eff2b17cd7a14bd8d6465de1b999f47
bobbanovski <robert.coleman1@uqconnect.edu.au>
2017-02-17 05:36:58 +0000

    Full CRUD operations for posts

--- flask_init/blog/views.py
+++ flask_init/blog/views.py
@@ -98,7 +98,7 @@ def post():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('article', slug=slug))
-    return render_template('blog/post.html', form=form)
+    return render_template('blog/post.html', form=form, action="new")
     
 @app.route('/article/<slug>')
 def article(slug):
@@ -106,12 +106,33 @@ def article(slug):
         
     return render_template('blog/article.html', post = post)
     
-@app.route('/edit/<int:post_id>')
+@app.route('/edit/<int:post_id>', methods=('GET', 'POST'))
 @author_required
 def edit(post_id):
     post = Post.query.filter_by(id=post_id).first_or_404()
     form = PostForm(obj=post) #assign all fields from post object
-    return render_template('blog/post.html', form=form, post = post, action="edit")
+    if form.validate_on_submit():
+        original_image=post.image
+        form.populate_obj(post)
+        if form.image.has_file():
+            image = request.files.get('image')
+            try:
+                filename = uploaded_images.save(image)
+            except:
+                flash('Unable to save image to database')
+            if filename:
+                post.image = filename
+        else:
+            post.image = original_image
+        if form.new_category.data:
+            new_category=Category(form.new_category.data)
+            db.session.add(new_category)
+            db.session.flush()
+            post.category = new_category
+        db.session.commit()
+        return redirect(url_for('article', slug=post.slug))
+        
+    return render_template('blog/post.html', form=form, post=post, action="edit")
 
 @app.route('/delete/<int:post_id>')
 @author_required
